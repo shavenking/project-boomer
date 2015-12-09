da1203236503f9e88302a06b14443d7cfff5995f
andytsai-shaven <andytsai.shaven@gmail.com>
2015-12-09 13:58:56 +0800

    Add migration of "workflow".

---
+++ database/migrations/2015_12_09_054836_create_workflows_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkflowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('worksflows', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('order')->unsigned();
+            $table->string('title');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('worksflows');
+    }
+}
