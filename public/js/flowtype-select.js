9ac1112a108c9e7ebbe69755587b387ef478e1ab
andytsai-shaven <andytsai.shaven@gmail.com>
2015-12-12 21:07:58 +0800

    Add FlowtypeSelect React Component.

---
+++ public/js/flowtype-select.js
@@ -0,0 +1,102 @@
+class FlowtypeSelect extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            mainflowTypes: [],
+            detailingflowTypes: [],
+            selectedMainflowTypeId: '',
+            selectedDetailingflowTypeId: ''
+        }
+
+        window.$.getJSON('/api/v1/mainflow-types', response => {
+            this.setState({
+                mainflowTypes: response.mainflow_types
+            })
+        })
+
+        this.onMainflowTypeSelected = this.onMainflowTypeSelected.bind(this)
+        this.onDetailingflowTypesSelected = this.onDetailingflowTypesSelected.bind(this)
+        this.clearDetailingSelection = this.clearDetailingSelection.bind(this)
+    }
+
+    componentDidMount() {
+        window.$(window.ReactDOM.findDOMNode(this)).find('.dropdown').dropdown({
+            message: {
+                noResults: '查無相關資料'
+            }
+        })
+
+        window.$(this._mainflowTypeInput).change(this.onMainflowTypeSelected)
+        window.$(this._detailingflowTypeInput).change(this.onDetailingflowTypesSelected)
+    }
+
+    onMainflowTypeSelected(e) {
+        var selectedId = e.target.value
+
+        this.setState({
+            selectedMainflowTypeId: selectedId
+        }, () => {
+            this.clearDetailingSelection()
+        })
+
+        window.$.getJSON(`/api/v1/mainflow-types/${selectedId}/detailingflow-types`, response => {
+            this.setState({
+                detailingflowTypes: response.detailingflow_types
+            }, () => {
+                var firstTypeId = this.state.detailingflowTypes.length ? this.state.detailingflowTypes[0].id : ''
+                window.$('#detailingflow-types-dropdown').dropdown('set selected', [firstTypeId])
+            })
+        })
+    }
+
+    onDetailingflowTypesSelected(e) {
+        var selectedId = e.target.value
+
+        this.setState({
+            selectedDetailingflowTypeId: selectedId
+        })
+    }
+
+    clearDetailingSelection() {
+        // line below will trigger change event on detailingflow_type_id input
+        // so the state is updated
+        window.$('#detailingflow-types-dropdown').dropdown('clear')
+    }
+
+    render() {
+        var mainflowTypeItems = this.state.mainflowTypes.map((mainflowType) => {
+            return <div className="item" data-value={ mainflowType.id }>{ mainflowType.name }</div>
+        })
+
+        var detailingflowTypeItems = this.state.detailingflowTypes.map(detailingflowType => {
+            return <div className="item" data-value={ detailingflowType.id }>{ detailingflowType.name }</div>
+        })
+
+        return (
+            <div className="grouped fields">
+                <div className="field">
+                    <div className="ui selection dropdown">
+                        <input type="hidden" name="mainflow_type_id" ref={ (c) => { this._mainflowTypeInput = c }} />
+                        <i className="dropdown icon"></i>
+                        <div className="default text">工程類別</div>
+                        <div className="menu">
+                            { mainflowTypeItems }
+                        </div>
+                    </div>
+                </div>
+
+                <div className="field">
+                    <div className={ window.classNames('ui', 'selection', 'dropdown', { disabled: !this.state.detailingflowTypes.length }) } id="detailingflow-types-dropdown">
+                        <input type="hidden" name="detailingflow_type_id" ref={ (c) => { this._detailingflowTypeInput = c }} />
+                        <i className="dropdown icon"></i>
+                        <div className="default text">工程順序</div>
+                        <div className="menu">
+                            { detailingflowTypeItems }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
