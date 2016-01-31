7a823785dbd9f9765e82b4795f3951479b50fe6a
andytsai-shaven <andytsai.shaven@gmail.com>
2016-01-31 17:51:07 +0800

    Rework settings' sidebar.

--- resources/views/components/settings-sidebar.blade.php
+++ resources/views/components/settings-sidebar.blade.php
@@ -1,30 +1,22 @@
-<div class="ui vertical menu">
-    <div class="item">
-        <div class="header">{{ trans('all.settings') }}</div>
+<div class="ui vertical pointing menu">
+    <a
+        href="{{ route('workflows.index') }}"
+        class="@if (str_is('workflows*', request()->route()->getName())) active @endif item"
+    >
+        {{ trans_choice('all.workflows', 2) }}
+    </a>
 
-        <div class="menu">
+    <a
+        href="{{ route('works.index') }}"
+        class="@if (str_is('works*', request()->route()->getName())) active @endif item"
+    >
+        {{ trans_choice('all.works', 2) }}
+    </a>
 
-            <a
-                href="{{ route('workflows.index') }}"
-                class="@if (str_is('workflows*', request()->route()->getName())) active @endif item"
-            >
-                {{ trans_choice('all.workflows', 2) }}
-            </a>
-
-            <a
-                href="{{ route('works.index') }}"
-                class="@if (str_is('works*', request()->route()->getName())) active @endif item"
-            >
-                {{ trans_choice('all.works', 2) }}
-            </a>
-
-            <a
-                href="{{ route('checklists.index') }}"
-                class="@if (str_is('checklists*', request()->route()->getName())) active @endif item"
-            >
-                {{ trans_choice('all.checklists', 2) }}
-            </a>
-
-        </div>
-    </div>
+    <a
+        href="{{ route('checklists.index') }}"
+        class="@if (str_is('checklists*', request()->route()->getName())) active @endif item"
+    >
+        {{ trans_choice('all.checklists', 2) }}
+    </a>
 </div>
