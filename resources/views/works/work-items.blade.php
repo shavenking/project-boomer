2d10d81f176c4f37577912e3f8782a8f5537e9ca
shavenking <shavenking@gmail.com>
2016-02-04 12:16:17 +0800

    Temperary useing the old component.

--- resources/views/works/work-items.blade.php
+++ resources/views/works/work-items.blade.php
@@ -15,20 +15,21 @@
         <a href="{{ route('works.checklist', $work->id) }}" class="item">{{ trans_choice('all.checklists', 1) }}</a>
     </div>
 
-    <div class="ui raised segment">
-        <statistics-workitems 
-            work-id="{{ $work->id }}"
-            total-price-label="{{ trans('all.total_price') }}"
-        ></statistics-workitems>
-    </div>
-
-    <table-workitems 
+    <workitem-list
         work-id="{{ $work->id }}"
-        cost-type-label="{{ trans('all.type') }}"
-        unit-label="{{ trans('all.unit') }}"
+        unit-label="{{ trans('all.select_unit') }}"
+        cost-type-label="{{ trans('all.select_cost_type') }}"
+        order-label="{{ trans('all.order') }}"
         name-label="{{ trans('all.name') }}"
         amount-label="{{ trans('all.amount') }}"
-        unit-price-label="{{ trans('all.unit_price') }}"
+        create-label="{{ trans('all.create') }}"
+        clear-label="{{ trans('all.clear') }}"
+        update-label="{{ trans('all.save') }}"
+        cancel-label="{{ trans('all.cancel') }}"
         total-price-label="{{ trans('all.total_price') }}"
-    ></table-workitems>
+        edit-text="{{ trans('all.edit') }}"
+        delete-text="{{ trans('all.delete') }}"
+        amount-text="{{ trans('all.amount') }}"
+        unit-price-text="{{ trans('all.unit_price') }}"
+    ></workitem-list>
 @stop
