9374f081ad7514d9d0aa221f7627e3bd3c848d66
tkps2266 <tkps2266@gmail.com>
2016-03-07 11:59:21 +0800

    加入判斷手機頁面格式

--- resources/views/layouts/settings.blade.php
+++ resources/views/layouts/settings.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
     <title>@include('components.title')</title>
     <link rel="stylesheet" href="/css/semantic.min.css">
 </head>
