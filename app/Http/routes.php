4a4f8b908526cc4f410da6ce126a5fb644c7236d
shavenking <shavenking@gmail.com>
2016-03-19 19:32:59 +0800

    Comment out incomplete routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,7 +20,7 @@ Route::group(['prefix' => 'api/v1'], function () {
     resource('checklists.checkitems', Checklists\CheckitemsController::class);
     resource('checklists', Checklists\ChecklistsController::class);
 
-     resource('subcontractors', Subconstractors\SubconstractorsController::class);
+    // resource('subcontractors', Subconstractors\SubconstractorsController::class);
 });
 
 Route::group(['middleware' => ['csrftoken', 'localize']], function () {
