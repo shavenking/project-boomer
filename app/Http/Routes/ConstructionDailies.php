e0381708c337d5d07a442cb5e2311b98c4be7d8a
shavenking <shavenking@gmail.com>
2016-03-20 10:10:20 +0800

    Rework ConstructionDailiesController.

--- app/Http/Routes/ConstructionDailies.php
+++ app/Http/Routes/ConstructionDailies.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::group(['middleware' => ['csrftoken', 'localize']], function () {
-    resource('projects.construction-dailies', Projects\ConstructionDailiesController::class);
+    resource('projects.construction-dailies', ConstructionDailiesController::class);
 });
