c28cec622b525dfd746b91355d4fd6a80726cb66
shavenking <shavenking@gmail.com>
2016-03-20 10:46:02 +0800

    Rework ProjectWorksController.

---
+++ app/Http/Controllers/ProjectWorksController.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ProjectWorksController extends Controller
+{
+    public function index($projectId, Request $request)
+    {
+        $mainflowTypeId = $request->query('mainflow_type_id');
+        $detailingflowTypeId = $request->query('detailingflow_type_id');
+
+        $project = \App\Entities\Project::findOrFail($projectId);
+
+        $query = \App\Entities\ProjectWork::query();
+
+        if (!empty($mainflowTypeId)) {
+            $query->whereHas('detailingflowType', function ($query) use ($mainflowTypeId) {
+                $query->whereMainflowTypeId($mainflowTypeId);
+            });
+
+            $mainflowTypeName = \App\Entities\MainflowType::findOrFail($mainflowTypeId)->name;
+        }
+
+        if (!empty($detailingflowTypeId)) {
+            $query->whereDetailingflowTypeId($detailingflowTypeId);
+
+            $detailingflowTypeName = \App\Entities\DetailingflowType::findOrFail($detailingflowTypeId)->name;
+        }
+
+        $works = $query->with('detailingflowType.mainflowType', 'unit')->get();
+
+        return response()->json(compact('works'));
+    }
+
+    public function indexOfWorkitems($projectId, $workId)
+    {
+        $workitems = \App\Entities\ProjectWorkitem::whereProjectWorkId($workId)->get();
+
+        return response()->json(compact('workitems'));
+    }
+
+    public function search($projectId, Request $request)
+    {
+        $project = \App\Entities\Project::findOrFail($projectId);
+
+        return view('project-works.search', compact('project'));
+    }
+
+    public function create($projectId)
+    {
+        $project = \App\Entities\Project::findOrFail($projectId);
+
+        return view('project-works.create')->withProject($project);
+    }
+
+    public function createOfWorkitems($projectId, $workId)
+    {
+        $project = \App\Entities\Project::findOrFail($projectId);
+        $work = \App\Entities\ProjectWork::findOrFail($workId);
+        $units = \App\Entities\Unit::all();
+        $costTypes = \App\Entities\CostType::all();
+
+        return view('project-workitems.create', compact('project', 'work', 'units', 'costTypes'));
+    }
+
+    public function store($projectId, Request $request)
+    {
+        $this->validate($request, [
+            'work_ids' => 'required'
+        ]);
+
+        $workIds = explode(',', $request->input('work_ids'));
+
+        $works = \App\Entities\Work::findOrFail($workIds);
+
+        $project = \App\Entities\Project::findOrFail($projectId);
+
+        $works->each(function ($work) use ($project) {
+            $projectWork = $project->works()->create($work->toArray());
+
+            $work->items->each(function ($item) use ($projectWork) {
+                $projectWork->workitems()->create($item->toArray());
+            });
+        });
+
+        if ($request->ajax()) {
+            return response()->json();
+        }
+
+        return redirect()->route('projects.bid.works', $projectId);
+    }
+
+    public function storeOfWorkitems($projectId, $workId, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'amount' => 'required',
+            'unit_price' => 'required',
+            'unit_id' => 'required',
+            'cost_type_id' => 'required'
+        ]);
+
+        $work = \App\Entities\ProjectWork::findOrFail($workId);
+
+        $workitem = $work->workitems()->create($request->all());
+        $work->reCalculateUnitPrice();
+
+        if ($request->ajax()) {
+            return response()->json(compact('workitem'));
+        }
+
+        return redirect()->route('projects.works.show', [$projectId, $workId]);
+    }
+
+    public function updateOfWorkitems($projectId, $workId, $workitemId, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'amount' => 'required',
+            'unit_price' => 'required'
+        ]);
+
+        $workitem = \App\Entities\ProjectWorkitem::findOrFail($workitemId);
+
+        $workitem->update($request->all());
+
+        $workitem->work->recalculateUnitPrice();
+
+        if ($request->ajax()) {
+            return response()->json(compact('workitem'));
+        }
+
+        return redirect()->route('projects.works.show', [$projectId, $workitem->work->id]);
+    }
+
+    public function show($projectId, $workId)
+    {
+        $project = \App\Entities\Project::findOrFail($projectId);
+        $work = \App\Entities\ProjectWork::findOrFail($workId);
+
+        return view('project-works.show')
+            ->withProject($project)
+            ->withWork($work);
+    }
+
+    public function destroy($projectId, $workId)
+    {
+        $work = \App\Entities\ProjectWork::findOrFail($workId);
+
+        \App\Entities\ProjectWorkitem::destroy($work->workitems()->lists('id')->all());
+
+        $work->delete();
+
+        return redirect()->route('projects.bid.works', $projectId);
+    }
+
+    public function destroyOfWorkitems($projectId, $workId, $workitemId, Request $request)
+    {
+        $workitem = \App\Entities\ProjectWorkitem::findOrFail($workitemId);
+
+        $work = \App\Entities\ProjectWork::findOrFail($workitem->getAttribute('project_work_id'));
+
+        $workitem->delete();
+        $work->reCalculateUnitPrice();
+
+        if ($request->ajax()) {
+            return response()->json();
+        }
+
+        return redirect()->route('projects.works.show', [$projectId, $work->id]);
+    }
+}
--- app/Http/Routes/ProjectWorks.php
+++ app/Http/Routes/ProjectWorks.php
@@ -1,10 +1,25 @@
 <?php
 Route::group(['prefix' => 'api/v1'], function () {
-    resource('projects.works.workitems', Projects\WorkitemsController::class);
-    resource('projects.works', Projects\WorksController::class);
+    get('projects/{projects}/works/{works}/workitems', ProjectWorksController::class . '@indexOfWorkitems');
+    get('projects/{projects}/works/{works}/workitems/create', ProjectWorksController::class . '@createOfWorkitems');
+    post('projects/{projects}/works/{works}/workitems', ProjectWorksController::class . '@storeOfWorkitems');
+    put('projects/{projects}/works/{works}/workitems/{workitems}', ProjectWorksController::class . '@updateOfWorkitems');
+    delete('projects/{projects}/works/{works}/workitems/{workitems}', ProjectWorksController::class . '@destroyOfWorkitems');
+
+    resource('projects.works', ProjectWorksController::class);
 });
 Route::group(['middleware' => ['csrftoken', 'localize']], function () {
-    get('projects/{project}/works/search', Projects\WorksController::class . '@search')->name('projects.works.search');
-    resource('projects.works.workitems', Projects\WorkitemsController::class);
-    resource('projects.works', Projects\WorksController::class);
+    get('projects/{projects}/works/{works}/workitems', ProjectWorksController::class . '@indexOfWorkitems')
+        ->name('projects.works.workitems.index');
+    get('projects/{projects}/works/{works}/workitems/create', ProjectWorksController::class . '@createOfWorkitems')
+        ->name('projects.works.workitems.create');
+    post('projects/{projects}/works/{works}/workitems', ProjectWorksController::class . '@storeOfWorkitems')
+        ->name('projects.works.workitems.store');
+    put('projects/{projects}/works/{works}/workitems/{workitems}', ProjectWorksController::class . '@updateOfWorkitems')
+        ->name('projects.works.workitems.update');
+    delete('projects/{projects}/works/{works}/workitems/{workitems}', ProjectWorksController::class . '@destroyOfWorkitems')
+        ->name('projects.works.workitems.destroy');
+
+    get('projects/{project}/works/search', ProjectWorksController::class . '@search')->name('projects.works.search');
+    resource('projects.works', ProjectWorksController::class);
 });
