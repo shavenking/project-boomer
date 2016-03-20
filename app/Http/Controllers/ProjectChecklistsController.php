c210e0fc796dcbf2efd6fd309cdfb5aec32ab86f
shavenking <shavenking@gmail.com>
2016-03-20 10:37:48 +0800

    Rework ProjectChecklistsController.

--- app/Http/Controllers/ProjectChecklistsController.php
+++ app/Http/Controllers/ProjectChecklistsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Projects;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
@@ -11,13 +11,8 @@ use App\Entities\Project;
 use App\Entities\Checklist;
 use App\Entities\ProjectChecklist;
 
-class ChecklistsController extends Controller
+class ProjectChecklistsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index($projectId)
     {
         $project = Project::findOrFail($projectId);
@@ -31,11 +26,6 @@ class ChecklistsController extends Controller
             ->withChecklists($checklists);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create($projectId)
     {
         $project = Project::findOrFail($projectId);
@@ -47,12 +37,6 @@ class ChecklistsController extends Controller
             ->withChecklists($checklists);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store($projectId, Request $request)
     {
         $this->validate($request, [
@@ -83,12 +67,6 @@ class ChecklistsController extends Controller
         return redirect()->route('projects.checklists.show', [$projectId, $checklist->id]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($projectId, $checklistId)
     {
         $project = Project::findOrFail($projectId);
@@ -98,29 +76,6 @@ class ChecklistsController extends Controller
         return view('project-checklists.show')
             ->withProject($project)
             ->withChecklist($checklist);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
     }
 
     public function updateCheckitemsResults($projectId, $checklistId, Request $request)
@@ -146,16 +101,5 @@ class ChecklistsController extends Controller
         });
 
         return redirect()->route('projects.checklists.show', [$projectId, $checklistId]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- app/Http/Routes/ProjectChecklists.php
+++ app/Http/Routes/ProjectChecklists.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::group(['middleware' => ['csrftoken', 'localize']], function () {
-    put('projects/{project}/checklists/{checklist}/checkresults', Projects\ChecklistsController::class . '@updateCheckitemsResults')->name('projects.checklists.checkresults.update');
-    resource('projects.checklists', Projects\ChecklistsController::class);
+    put('projects/{project}/checklists/{checklist}/checkresults', ProjectChecklistsController::class . '@updateCheckitemsResults')->name('projects.checklists.checkresults.update');
+    resource('projects.checklists', ProjectChecklistsController::class);
 });
