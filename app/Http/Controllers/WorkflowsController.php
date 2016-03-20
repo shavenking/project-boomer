a0317247abf47476e26e8f83321bab70fb45e3af
shavenking <shavenking@gmail.com>
2016-03-20 11:26:57 +0800

    Rework WorkflowsController.

---
+++ app/Http/Controllers/WorkflowsController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Entities\Work;
+use App\Entities\Workflow;
+use App\Entities\WorkflowNode;
+
+class WorkflowsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $workflows = Workflow::all();
+
+        if ($request->ajax()) {
+            return response()->json(compact('workflows'));
+        }
+
+        return view('workflows.index')->withWorkflows($workflows);
+    }
+
+    public function indexOfNodes($workflowId)
+    {
+        $workflow = Workflow::with('nodes')->findOrFail($workflowId);
+
+        $nodes = $workflow->nodes;
+
+        return response()->json(compact('workflow', 'nodes'));
+    }
+
+    public function create()
+    {
+        return view('workflows.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+
+        $workflow = Workflow::create([
+            'name' => $request->name
+        ]);
+
+        if ($request->has('work_ids')) {
+            $workIds = explode(',', $request->input('work_ids'));
+            $works = Work::findOrFail($workIds);
+
+            foreach ($works as $work) {
+                $work->update(['workflow_id' => $workflow->id]);
+            }
+        }
+
+        if ($request->ajax()) {
+            return response()->json(compact('workflow'));
+        }
+
+        return redirect()->route('workflows.show', $workflow->id);
+    }
+
+    public function storeOfNodes($workflowId, Request $request)
+    {
+        $node = Workflow::findOrFail($workflowId)
+            ->nodes()
+            ->create([
+                'order' => $request->order,
+                'title' => $request->title
+            ]);
+
+        return response()->json(compact('node'));
+    }
+
+    public function show($id, Request $request)
+    {
+        $workflow = Workflow::findOrFail($id);
+
+        if ($request->ajax()) {
+            return response()->json(compact('workflow'));
+        }
+
+        return view('workflows.show')->withWorkflow($workflow);
+    }
+
+    public function showOfNodes($workflowId, $id)
+    {
+        $node = WorkflowNode::whereWorkflowId($workflowId)
+            ->whereId($id)
+            ->firstOrFail();
+
+        return response()->json(compact('node'));
+    }
+
+    public function updateOfNodes($workflowId, $id, Request $request)
+    {
+        $node = WorkflowNode::whereWorkflowId($workflowId)
+            ->whereId($id)
+            ->firstOrFail();
+
+        $node->update([
+            'order' => $request->order,
+            'title' => $request->title
+        ]);
+
+        return response()->json(compact('node'));
+    }
+
+    public function destroy($id, Request $request)
+    {
+        $workflow = Workflow::findOrFail($id);
+
+        WorkflowNode::destroy($workflow->nodes()->lists('id')->all());
+
+        $workflow->delete();
+
+        if ($request->ajax()) {
+            return response()->json();
+        }
+
+        return redirect()->back();
+    }
+
+    public function destroyOfNodes($workflowId, $id)
+    {
+        $node = WorkflowNode::whereWorkflowId($workflowId)
+            ->whereId($id)
+            ->firstOrFail();
+
+        $node->delete();
+
+        return response()->json();
+    }
+
+    public function checklist($workflowId)
+    {
+        $workflow = app(\App\Entities\Workflow::class)->findOrFail($workflowId);
+
+        if (!$workflow->checklist) {
+            return redirect()->route('checklists.create', ['workflow_id' => $workflow->id]);
+        }
+
+        return redirect()->route('checklists.show', $workflow->checklist->id);
+    }
+
+    public function works($id)
+    {
+        $workflow = app(\App\Entities\Workflow::class)->findOrFail($id);
+
+        $works = app(\App\Entities\Work::class)->whereWorkflowId($workflow->id)->get();
+
+        return view('workflows.works')->withWorkflow($workflow)->withWorks($works);
+    }
+}
--- app/Http/Routes/Workflows.php
+++ app/Http/Routes/Workflows.php
@@ -1,11 +1,15 @@
 <?php
 Route::group(['prefix' => 'api/v1'], function () {
-    resource('workflows', Workflows\WorkflowsController::class);
-    resource('workflows.nodes', Workflows\NodesController::class);
+    get('workflows/{workflows}/nodes', WorkflowsController::class . '@indexOfNodes');
+    get('workflows/{workflows}/nodes/{nodes}', WorkflowsController::class . '@showOfNodes');
+    post('workflows/{workflows}/nodes', WorkflowsController::class . '@storeOfNodes');
+    put('workflows/{workflows}/nodes/{nodes}', WorkflowsController::class . '@updateOfNodes');
+    delete('workflows/{workflows}/nodes/{nodes}', WorkflowsController::class . '@destroyOfNodes');
+    resource('workflows', WorkflowsController::class);
 });
 
 Route::group(['middleware' => ['csrftoken', 'localize']], function () {
-    get('workflows/{workflow}/checklist', Workflows\WorkflowViewsController::class . '@checklist')->name('workflows.checklist');
-    get('workflows/{workflow}/works', Workflows\WorkflowViewsController::class . '@works')->name('workflows.works');
-    resource('workflows', Workflows\WorkflowViewsController::class);
+    get('workflows/{workflow}/checklist', WorkflowsController::class . '@checklist')->name('workflows.checklist');
+    get('workflows/{workflow}/works', WorkflowsController::class . '@works')->name('workflows.works');
+    resource('workflows', WorkflowsController::class);
 });
