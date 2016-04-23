3282dcef28a9d57b794fefb990b7547dde12b087
shavenking <shavenking@gmail.com>
2016-04-23 09:39:31 +0800

    Fix bug.

--- resources/views/project-constructiondailies/show.blade.php
+++ resources/views/project-constructiondailies/show.blade.php
@@ -2,7 +2,7 @@
     trans_choice('all.projects', 2) => route('projects.index'),
     "{$project->name}" => route('projects.show', $project->id),
     trans('all.external') => route('projects.external.index', $project->id),
-    trans('all.p_construction_daily') => route('projects.show', $project->id),
+    trans('all.p_construction_daily') => route('projects.construction-dailies.index', $project->id),
     trans('all.construction_daily') => null
 ] /* --}}
 
