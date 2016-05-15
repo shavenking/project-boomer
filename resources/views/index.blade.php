6aef34d926453b0d58320f98948a0390a8038cf3
tkps2266 <tkps2266@gmail.com>
2016-05-15 16:24:21 +0800

    增加首頁圖連結

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,5 @@
 @extends('layouts.general-nosidebar')
 
 @section('content')
-    <img src="/首頁圖二.jp2" class="ui centered large rounded image" alt="">
+   <a href="{{ route('projects.index') }}"><img src="/首頁圖二.jp2" class="ui centered large rounded image" alt=""></a>
 @stop
