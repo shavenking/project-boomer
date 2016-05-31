8de26b66fb17c87327bff6474d6cfa30bffdfcb6
tkps2266 <tkps2266@gmail.com>
2016-05-31 20:20:40 +0800

    更新首頁圖

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,5 @@
 @extends('layouts.general-nosidebar')
 
 @section('content')
-   <a href="{{ route('projects.index') }}"><img src="/home3.jpg" class="ui centered large rounded image" alt=""></a>
+   <a href="{{ route('projects.index') }}"><img src="/首頁圖.jpg" class="ui centered large rounded image" alt=""></a>
 @stop
