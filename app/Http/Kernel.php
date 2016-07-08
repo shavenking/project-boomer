7b3d0de617fffc5413e160d36a2ab7d98cd19475
shavenking <shavenking@gmail.com>
2016-07-08 13:41:39 +0800

    Clean up codebase.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,9 +29,6 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'csrftoken' => \App\Http\Middleware\VerifyCsrfToken::class,
-        'role' => \Zizaco\Entrust\Middleware\EntrustRole::class,
-        'permission' => \Zizaco\Entrust\Middleware\EntrustPermission::class,
-        'ability' => \Zizaco\Entrust\Middleware\EntrustAbility::class,
         'localize' => \App\Http\Middleware\Localize::class,
         'project.manager' => \App\Http\Middleware\ProjectManager::class,
         'user.in.project' => \App\Http\Middleware\UserInProject::class
