bcfb11b6933423d32408b6a72bdaed6fd451daff
shavenking <shavenking@gmail.com>
2016-07-08 17:19:07 +0800

    Add Authorization to ProjectChecklist.

--- app/Http/Routes/ConstructionDailies.php
+++ app/Http/Routes/ConstructionDailies.php
@@ -1,5 +1,8 @@
 <?php
 
 Route::group(['middleware' => ['csrftoken']], function () {
-    resource('projects.construction-dailies', ConstructionDailiesController::class);
+    $controller = ConstructionDailiesController::class;
+
+    Route::get('projects/{projects}/construction-dailies/{date}', "$controller@show")->name('projects.construction-dailies.show');
+    Route::get('projects/{projects}/construction-dailies', "$controller@index")->name('projects.construction-dailies.index');
 });
