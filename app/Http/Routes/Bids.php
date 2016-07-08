1b80b2835a724efb24d16493432ffa00708c7c87
shavenking <shavenking@gmail.com>
2016-07-08 17:01:52 +0800

    Add Authorization to ProjectWork.

--- app/Http/Routes/Bids.php
+++ app/Http/Routes/Bids.php
@@ -1,6 +1,5 @@
 <?php
 
 Route::group(['middleware' => ['csrftoken']], function () {
-    get('projects/{project}/bid', BidsController::class . '@index')->name('projects.bid.index');
-    get('projects/{project}/bid/works', BidsController::class . '@works')->name('projects.bid.works');
+    Route::get('projects/{project}/bid/works', BidsController::class . '@works')->name('projects.bid.works');
 });
