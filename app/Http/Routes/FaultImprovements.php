c71ef8ab8d631cd0d39c4e51486dd67ca100f761
shavenking <shavenking@gmail.com>
2016-07-08 17:36:13 +0800

    Add Authorization to FaultImprovement.

--- app/Http/Routes/FaultImprovements.php
+++ app/Http/Routes/FaultImprovements.php
@@ -1,10 +1,14 @@
 <?php
 
-Route::group(['prefix' => 'api/v1'], function () {
-    put('projects/{projectId}/fault-improvements/{faultImprovementId}', FaultImprovementsController::class . '@update');
-    post('projects/{projectId}/fault-improvements/{faultImprovementId}/photos/{step}', FaultImprovementsController::class . '@storePhotos');
+$controller = FaultImprovementsController::class;
+
+Route::group(['prefix' => 'api/v1'], function () use ($controller) {
+    Route::put('projects/{projects}/fault-improvements/{faultImprovements}', "$controller@update")->middleware('role:quality_manager|field_engineer');
+    Route::post('projects/{projects}/fault-improvements/{faultImprovements}/photos/{step}', "$controller@storePhotos")->middleware('role:quality_manager|field_engineer');
 });
 
-Route::group(['middleware' => ['csrftoken']], function () {
-    resource('projects.fault-improvements', FaultImprovementsController::class);
+Route::group(['middleware' => ['csrftoken']], function () use ($controller) {
+    Route::get('projects/{projects}/fault-improvements', "$controller@index")->name('projects.fault-improvements.index');
+    Route::get('projects/{projects}/fault-improvements/{faultImprovements}', "$controller@show")->name('projects.fault-improvements.show');
+    Route::put('projects/{projects}/fault-improvements/{faultImprovements}', "$controller@update")->name('projects.fault-improvements.update')->middleware('role:quality_manager|field_engineer');
 });
