4d2b2f4dbc1b8b3f64335885d6b7a71b54b4d071
tkps2266 <tkps2266@gmail.com>
2016-08-04 09:48:52 +0800

    名稱校正

--- resources/views/components/project-setting-sidebar.blade.php
+++ resources/views/components/project-setting-sidebar.blade.php
@@ -1,10 +1,10 @@
-<h3 class="header">設定</h3>
+<h3 class="header">權限管理</h3>
 <div class="ui fluid vertical buttons">
     <a
         href="{{ route('projects.members.index', $project->id) }}"
         class="ui @if (preg_match('/projects\.(members).*/', request()->route()->getName())) active @endif button"
     >
-        人員管理
+        專案成員管理
     </a>
 </div>
 
