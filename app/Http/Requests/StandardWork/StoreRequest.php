69339c9605c4884f685e2015c918770dfb16577b
shavenking <shavenking@gmail.com>
2017-06-04 20:50:43 +0800

    Fix bug

--- app/Http/Requests/StandardWork/StoreRequest.php
+++ app/Http/Requests/StandardWork/StoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 
-class StoreWorkRequest extends Request
+class StoreRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
